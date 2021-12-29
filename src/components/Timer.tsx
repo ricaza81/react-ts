9a4a027f12c2c26ee630bb10044643495d92bcbb
ricaza81 <ricaza81@gmail.com>
2021-12-28 21:48:47 -0500

    userRef

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,11 @@
 import { useState, useEffect } from 'react';
-export const Timer = () => {	
+
+type TimerArs = {
+	milisegundos:number
+}
+
+
+export const Timer = ( { milisegundos }:TimerArs ) => {	
 	const [segundos, setSegundos] = useState(0)
 	useEffect( () => {
 		setInterval( () => setSegundos( segundos => segundos + 1 ) , 1000 );
--- src/components/TimerPadre.tsx
+++ src/components/TimerPadre.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { Timer } from './Timer';
 
 export const TimerPadre = () => {
 	
-
+const [millisegundos, setMillisegundos] = useState(1000);
 
 	return (
-		<div>
+		<>
 			<h3 className="mt-5">useEffect - UseRef</h3>
-			<span>Milisegundos { 1000 }</span>
-			<Timer />
-		</div>
+			<span>Milisegundos { millisegundos }</span>
+
+			<button
+				onClick={ () => setMillisegundos(1000) }
+				className="btn btn-outline-primary mt-2">1000
+			</button>
+
+			<button
+				onClick={ () => setMillisegundos(2000) }
+				className="btn btn-outline-primary mt-2">2000
+			</button>
+
+
+			<Timer milisegundos = { millisegundos } />
+		</>
 	)
 }
